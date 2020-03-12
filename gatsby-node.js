ee7ec60b5363c7c0c0d8beb2bdfcba0384733684
⛏ <maxsteenbergen@gmail.com>
2020-03-12 19:24:00 +0100

    Remove unneeded cruft, update metadate

---
+++ gatsby-node.js
@@ -0,0 +1,110 @@
+const googleapi = require(`./googleapis`);
+const path = require(`path`);
+const mkdirp = require(`mkdirp`);
+const fs = require(`fs`);
+
+const log = str => console.log(`\n\n🚗 `, str);
+const FOLDER = `application/vnd.google-apps.folder`;
+const GOOGLE_DOC = 'application/vnd.google-apps.document';
+const GOOGLE_SHEET = 'application/vnd.google-apps.spreadsheet';
+
+let shouldExportGDocs;
+let shouldExportGSheets;
+let exportDocMime;
+let exportSheetMime;
+let middleware;
+
+exports.onPreBootstrap = ({ graphql, actions }, { folderId, keyFile, key, destination, exportGDocs, exportDocMimeType, exportGSheets, exportSheetMimeType, exportMiddleware }) => {
+  return new Promise(async resolve => {
+    log(`Started downloading content`);
+
+    // Get token and fetch root folder.
+    const token = keyFile ? await googleapi.getToken({ keyFile }) : await googleapi.getToken({ key });
+    const cmsFiles = await googleapi.getFolder(folderId, token);
+    shouldExportGDocs = exportGDocs;
+    shouldExportGSheets = exportGSheets;
+    exportDocMime = exportDocMimeType;
+    exportSheetMime = exportSheetMimeType;
+    middleware = exportMiddleware === undefined ? x => x : exportMiddleware;
+
+    // Create content directory if it doesn't exist.
+    mkdirp(destination);
+
+    // Start downloading recursively through all folders.
+    console.time(`Downloading content`);
+    recursiveFolders(cmsFiles, undefined, token, destination).then(() => {
+      console.timeEnd(`Downloading content`);
+      resolve();
+    });
+  });
+};
+
+function recursiveFolders(array, parent = '', token, destination) {
+  return new Promise(async (resolve, reject) => {
+    let promises = [];
+    let filesToDownload = array;
+
+    for (let file of filesToDownload) {
+      // Check if it`s a folder or a file
+      if (file.mimeType === FOLDER) {
+        // If it`s a folder, create it in filesystem
+        log(`Creating folder ${parent}/${file.name}`);
+        mkdirp(path.join(destination, parent, file.name));
+
+        // Then, get the files inside and run the function again.
+        const files = await googleapi.getFolder(file.id, token);
+        promises.push(recursiveFolders(files, `${parent}/${file.name}`, token, destination));
+      } else {
+        promises.push(new Promise(async (resolve, reject) => {
+          // If it`s a file, download it and convert to buffer.
+          const dest = path.join(destination, parent, getFilenameByMime(file));
+
+          if (fs.existsSync(dest)) {
+            resolve(getFilenameByMime(file));
+            return log(`Using cached ${getFilenameByMime(file)}`);
+          }
+
+          let buffer;
+          switch (file.mimeType) {
+            case GOOGLE_DOC:
+              buffer = shouldExportGDocs ? await middleware(googleapi.getGDoc(file.id, token, exportDocMime)) : null;
+              break;
+            case GOOGLE_SHEET:
+              buffer = shouldExportGSheets ? await middleware(googleapi.getGDoc(file.id, token, exportSheetMime)) : null;
+              break;
+            default:
+              buffer = await googleapi.getFile(file.id, token);
+              break;
+          }
+
+          if (buffer === null) {
+            resolve();
+          } else {
+            // Finally, write buffer to file.
+            fs.writeFile(dest, buffer, err => {
+              if (err || buffer === null) return log(err);
+              resolve(getFilenameByMime(file));
+              return log(`Saved file ${getFilenameByMime(file)}`);
+            });
+          }
+        }));
+      }
+    }
+
+    return Promise.all(promises).then(() => {
+      resolve();return log('all done');
+    });
+  });
+}
+
+const fileExtensionsByMime = new Map([['text/html', '.html'], ['application/zip', '.zip'], ['text/plain', '.txt'], ['application/rtf', '.rtf'], ['application/vnd.oasis.opendocument.text', '.odt'], ['application/pdf', '.pdf'], ['application/vnd.openxmlformats-officedocument.wordprocessingml.document', '.docx'], ['application/epub+zip', '.epub'], ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', '.xlsx']]);
+
+const getFilenameByMime = file => {
+  if (file.mimeType === GOOGLE_DOC) {
+    return `${file.name}${fileExtensionsByMime.get(exportDocMime)}`;
+  } else if (file.mimeType === GOOGLE_SHEET) {
+    return `${file.name}${fileExtensionsByMime.get(exportSheetMime)}`;
+  } else {
+    return file.name;
+  }
+};
---
+++ googleapis.js
@@ -0,0 +1,101 @@
+const { GoogleToken } = require('gtoken');
+const request = require('request');
+
+const getToken = ({ keyFile, key }) => {
+  return new Promise((resolve, reject) => {
+    const scope = ['https://www.googleapis.com/auth/drive'];
+    const gtoken = keyFile ? new GoogleToken({
+      keyFile,
+      scope: scope
+    }) : new GoogleToken({
+      email: key.client_email,
+      scope: scope,
+      key: key.private_key.replace(/(\\r)|(\\n)/g, '\n')
+    });
+
+    gtoken.getToken((err, token) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(token);
+      }
+    });
+  });
+};
+
+const getFolder = (folderId, token) => {
+  return new Promise((resolve, reject) => {
+    request({
+      uri: `https://www.googleapis.com/drive/v3/files`,
+      auth: {
+        bearer: token
+      },
+      qs: {
+        q: `'${folderId}' in parents`
+      }
+    }, (err, res, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.parse(body).files);
+      }
+    });
+  });
+};
+
+const getFile = (fileId, token) => {
+  return new Promise((resolve, reject) => {
+    requestFile(resolve, reject, fileId, token, 1100);
+  });
+};
+
+const getGDoc = (fileId, token, mimeType) => {
+  return new Promise((resolve, reject) => {
+    request({
+      uri: `https://www.googleapis.com/drive/v3/files/${fileId}/export`,
+      auth: {
+        bearer: token
+      },
+      encoding: null,
+      qs: {
+        mimeType: mimeType
+      }
+    }, (err, res, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(body);
+      }
+    });
+  });
+};
+
+module.exports = {
+  getToken,
+  getFolder,
+  getFile,
+  getGDoc
+};
+
+function requestFile(resolve, reject, fileId, token, delay) {
+  request({
+    uri: `https://www.googleapis.com/drive/v3/files/${fileId}`,
+    auth: {
+      bearer: token
+    },
+    encoding: null,
+    qs: {
+      alt: 'media'
+    }
+  }, (err, res, body) => {
+    if (err) {
+      reject(err);
+    } else if (res.statusCode == 403) {
+      setTimeout(() => {
+        requestFile(resolve, reject, fileId, token, delay * 2);
+      }, delay * 2);
+    } else {
+      resolve(body);
+    }
+  });
+};
